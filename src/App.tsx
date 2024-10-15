2eebd764282bde15dcd604c89d08742ab42eab76
Luiz Vinicius <viniciusdgba2008@gmail.com>
2024-10-14 21:08:33 -0300

    estudo de reducer recebendos dado de uma api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useReducer } from "react";
+import "./App.css";
+import axios from "axios";
+
+type StateProps<T> = {
+	data?: T;
+	error?: Error;
+	isLoading: boolean;
+};
+
+type ActionProps<T> =
+	| {
+			type: "loading";
+	  }
+	| {
+			type: "fetched";
+			payload: T;
+	  }
+	| {
+			type: "error";
+			payload: Error;
+	  };
+
+function useFetch<T = unknown>(url: string) {
+	const initialState: StateProps<T> = {
+		data: undefined,
+		error: undefined,
+		isLoading: true,
+	};
+
+	const reducer = (state: StateProps<T>, action: ActionProps<T>) => {
+		switch (action.type) {
+			case "loading":
+				return { ...initialState, isLoading: true };
+			case "fetched":
+				return { ...initialState, data: action.payload, isLoading: false };
+			case "error":
+				return { ...initialState, error: action.payload, isLoading: false };
+			default:
+				return state;
+		}
+	};
+
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const handleFetch = useCallback(async () => {
+		dispatch({ type: "loading" });
+
+		try {
+			const response = await axios.get(url);
+			dispatch({ type: "fetched", payload: response.data });
+		} catch (err) {
+			if (axios.isAxiosError(err) && err.response)
+				dispatch({ type: "error", payload: err as Error });
+		}
+	}, [url]);
+
+	useEffect(() => {
+		handleFetch();
+	}, [handleFetch]);
+
+	return {
+		data: state.data,
+		error: state.error,
+		isLoading: state.isLoading,
+	};
+}
+type DataProps = {
+	results: Array<{
+		name: {
+			first: string;
+			last: string;
+		};
+		cell: string;
+	}>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+	const { data, error, isLoading } = useFetch<DataProps>(
+		"https://randomuser.me/api/?results=20",
+	);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			{isLoading && <div>Carregando....</div>}
+			{error && <div>{error.message}</div>}
+
+			{data?.results?.map((item) => (
+				<div key={item.cell}>{item.name.first}</div>
+			))}
+		</>
+	);
 }
 
-export default App
+export default App;
